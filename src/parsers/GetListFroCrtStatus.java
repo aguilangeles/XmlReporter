295c1025f1079380e5e349dd5f13b4c71e4ae260
MUTNPROD003 <MUTNPROD003@UTNPROD003>
2013-09-19 14:12:30 -0300

    reduzco cuatro listas a una

---
+++ src/parsers/GetListFroCrtStatus.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parsers;
+
+import clases.Campo;
+import clases.Caratula;
+import clases.Metadato;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ *
+ * @author aguilangeles@gmail.com
+ */
+public class GetListFroCrtStatus {
+
+  private List<Caratula> caratulaList;
+
+  public GetListFroCrtStatus(List<Caratula> caratulaList) {
+    this.caratulaList = caratulaList;
+  }
+
+  public List<String> getStatusFromCrt(List<String> listaC1) {
+       List<String> listaValorC1 = new ArrayList();
+        String ret = "";
+        Iterator<Caratula> it = caratulaList.iterator();
+        while (it.hasNext()) {
+            Caratula caratula = it.next();
+            Metadato metadato = caratula.getMetadato();
+            Campo campo = metadato.getCampoByName("Id");
+         for (String c1 : listaC1)
+           {
+           if (campo.getValue().equalsIgnoreCase(c1) && campo != null) {
+               ret = metadato.toString();
+               listaValorC1.add(ret);
+           }
+           }
+        }
+        return listaValorC1;
+
+    }
+}
